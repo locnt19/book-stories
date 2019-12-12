5df3a7fc6c74cc024962b704441262919fed523e
Loc Thanh <locnt19@gmail.com>
2019-12-12 16:33:40 +0700

    ✨ Initial partial slide

--- src/components/main.js
+++ src/components/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
 	// all script write here
+	$('.slide_keyword').slick({
+		infinite: true,
+		slidesToShow: 10,
+		slidesToScroll: 2
+	});
+	$('.slide_book').slick({
+		infinite: true,
+		slidesToShow: 5
+	});
 });
