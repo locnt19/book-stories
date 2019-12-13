4c7c87c71d8504741b5feff28164b046b7f5a200
Hieu <hieu159vnvn@gmail.com>
2019-12-13 17:04:41 +0700

    index book

--- src/components/main.js
+++ src/components/main.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
 	$('.slide_book').slick({
 		infinite: true,
 		slidesToShow: 5,
-		slidesToScroll: 2
+		slidesToScroll: 2,
+		nextArrow:'<div class="book-btnnext"><i class="fas fa-caret-right"></i></i></div>',
+		prevArrow:'<div class="book-btnpre"><i class="fas fa-caret-left"></i></div>'
 	});
 });
