f75ebfee97f8f23eea2ad304460e7e47e45e2e26
hieu159vnvn <hieu159vnvn@gmail.com>
2019-12-14 00:40:19 +0700

    index+tusach+listbook

--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,14 @@ $(document).ready(function () {
 		infinite: true,
 		slidesToShow: 5,
 		slidesToScroll: 2,
-		nextArrow:'<div class="book-btnnext"><i class="fas fa-caret-right"></i></i></div>',
+		nextArrow:'<div class="book-btnnext"><i class="fas fa-caret-right"></i></div>',
 		prevArrow:'<div class="book-btnpre"><i class="fas fa-caret-left"></i></div>'
+	});
+	$('.banner_doitac').slick({
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		nextArrow:false,
+		prevArrow:false
 	});
 });
